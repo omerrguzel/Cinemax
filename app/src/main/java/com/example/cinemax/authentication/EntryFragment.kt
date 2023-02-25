51f00da34e68f29b593e78a90b3da2cf9fdbf116
Ömer GÜZEL <58399384+omerrguzel@users.noreply.github.com>
2023-02-25 18:07:39 +0300

    Facebook authentication is now functional

--- app/src/main/java/com/example/cinemax/authentication/EntryFragment.kt
+++ app/src/main/java/com/example/cinemax/authentication/EntryFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cinemax.authentication
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -20,8 +21,22 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.auth.GoogleAuthProvider
+import java.util.*
 
 class EntryFragment : Fragment() {
+
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+
+    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+    private var showOneTapUI = true
 
     private lateinit var binding : FragmentEntryBinding
     private lateinit var callbackManager: CallbackManager
@@ -29,7 +44,8 @@ class EntryFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FacebookSdk.sdkInitialize(requireContext())
-        callbackManager = CallbackManager.Factory.create()
+        auth = Firebase.auth
+
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,8 +56,7 @@ class EntryFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-        auth = Firebase.auth
+        callbackManager = CallbackManager.Factory.create()
 
         with(binding){
             signUpButton.setOnClickListener {
@@ -50,21 +65,44 @@ class EntryFragment : Fragment() {
             textViewLoginEntry.setOnClickListener {
                 findNavController().navigate(R.id.action_entryFragment_to_loginFragment)
             }
-            loginButton.setPermissions("email","public profile")
-            loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-                override fun onSuccess(loginResult: LoginResult) {
-                    handleFacebookAccessToken(loginResult.accessToken)
-                }
+            logoFacebook.setOnClickListener {
+                with(LoginManager.getInstance()){
+                    logInWithReadPermissions(this@EntryFragment,
+                        listOf("email","public_profile"))
+                    registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                        override fun onSuccess(loginResult: LoginResult) {
+                            Log.d(TAG, "facebook:onSuccess:$loginResult")
+                            handleFacebookAccessToken(loginResult.accessToken)
+                        }
 
-                override fun onCancel() {
-                    Log.d(TAG,"Cancel")
-                }
+                        override fun onCancel() {
+                            Log.d(TAG, "facebook:onCancel")
+                        }
 
-                override fun onError(exception: FacebookException) {
-                    Log.d(TAG,"Error")
+                        override fun onError(exception: FacebookException) {
+                            Log.d(TAG,"Error")
+                        }
+                    })
                 }
-            })
+            }
         }
+        oneTapClient = Identity.getSignInClient(requireActivity())
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                .setSupported(true)
+                .build())
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.web_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build()
+
     }
 
     private fun handleFacebookAccessToken(token: AccessToken) {
@@ -87,12 +125,50 @@ class EntryFragment : Fragment() {
             }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+
+        val googleCredential = oneTapClient.getSignInCredentialFromIntent(data)
+        val idToken = googleCredential.googleIdToken
+        when {
+            idToken != null -> {
+                // Got an ID token from Google. Use it to authenticate
+                // with Firebase.
+                val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                auth.signInWithCredential(firebaseCredential)
+                    .addOnCompleteListener(requireActivity()) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success")
+                            val user = auth.currentUser
+                            updateUI(user)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.exception)
+                            updateUI(null)
+                        }
+                    }
+            }
+            else -> {
+                // Shouldn't happen.
+                Log.d(TAG, "No ID token!")
+            }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
     private fun updateUI(user: FirebaseUser?) {
         if(user!=null){
-            Toast.makeText(context,"Please sign in to continue.",Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_entryFragment_to_homeFragment)
         }
         else{
-            findNavController().navigate(R.id.action_entryFragment_to_homeFragment)
+            Toast.makeText(context,"Please sign in to continue.",Toast.LENGTH_SHORT).show()
         }
     }
 }
